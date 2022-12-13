e6a0fb6baa044868de914f76b85b0befb1679ec5
MrKyroshi <mrkyroshi@gmail.com>
2022-12-13 15:36:10 +0100

    Authentique

---
+++ src/Form/AvisType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Avis;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AvisType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('note')
+            ->add('commentaire')
+            ->add('date')
+            ->add('utilisateur')
+            ->add('produit')
+            ->add('note', ChoiceType::class, [
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4'=>4,
+                    '5'=>5
+                ]])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Avis::class,
+        ]);
+    }
+}
